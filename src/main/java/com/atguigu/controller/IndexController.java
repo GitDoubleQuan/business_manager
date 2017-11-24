6a807fe5432459217447b57af8c24a7a71dc318b
Shuangquan.Xu <doublequan_job@163.com>
2017-11-24 19:47:07 +0800

    merge dev

--- src/main/java/com/atguigu/controller/IndexController.java
+++ src/main/java/com/atguigu/controller/IndexController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-
+    //dev
     @RequestMapping("index")
     public String index(ModelMap map, String url, String title, String success) {
         map.put("url", url);
